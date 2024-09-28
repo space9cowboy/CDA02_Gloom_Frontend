2b2645d771c7782119e73b97fde55575721329ca
Loic RABEARIVONY <lrabearivony@LWFRMB24036.local>
2024-09-28 22:57:13 +0200

    ajout navbar dans la page principale et mise en place page instruments

--- gloom/app/page.tsx
+++ gloom/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { Title } from "@/components/Title";
+import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center min-h-screen gap-16">
+      <Header />
       <Title />
     </div>
   );
